05a6d019b534c9d285ebfc7430ee13cb28e41b9d
David Neustadt <d.neustadt@shopware.com>
2022-11-28 17:34:54 +0000

    NEXT-23611 - remove flag FEATURE_NEXT_13810

--- Resources/app/administration/src/module/sw-promotion-v2/helper/promotion.helper.js
+++ Resources/app/administration/src/module/sw-promotion-v2/helper/promotion.helper.js
@@ -1,9 +1,8 @@
 import { hasOwnProperty } from 'src/core/service/utils/object.utils';
 
 /**
- * @deprecated tag:v6.5.0 - will be removed, use `sw-promotion-v2` instead
+ * @private
  */
-// eslint-disable-next-line sw-deprecation-rules/private-feature-declarations
 export const DiscountTypes = {
     PERCENTAGE: 'percentage',
     ABSOLUTE: 'absolute',
@@ -12,9 +11,8 @@ export const DiscountTypes = {
 };
 
 /**
- * @deprecated tag:v6.5.0 - will be removed, use `sw-promotion-v2` instead
+ * @private
  */
-// eslint-disable-next-line sw-deprecation-rules/private-feature-declarations
 export const DiscountScopes = {
     CART: 'cart',
     DELIVERY: 'delivery',
@@ -23,17 +21,15 @@ export const DiscountScopes = {
 };
 
 /**
- * @deprecated tag:v6.5.0 - will be removed, use `sw-promotion-v2` instead
+ * @private
  */
-// eslint-disable-next-line sw-deprecation-rules/private-feature-declarations
 export const PromotionPermissions = {
     isEditingAllowed,
 };
 
-
 /**
  * Gets if the editing of the promotion is allowed,
- * or if its forbidden to change anything because
+ * or if it's forbidden to change anything because
  * it has already been used.
  *
  * @param {Object} promotion
@@ -53,4 +49,3 @@ function isEditingAllowed(promotion) {
 
     return !promotion.hasOrders;
 }
-
