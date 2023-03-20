8e39b70adb3a71e9ba5151867fc7faf64131df6a
shopwareBot <shopwarebot@shopware.com>
2023-03-20 09:22:22 +0000

    NEXT-25117 - Release v6.5.0.0-rc2 -----BEGIN PGP SIGNATURE-----
    iQIzBAABCgAdFiEEhp3KWwxkm/eLMJCvMvVgzjOb3BgFAmQYIrsACgkQMvVgzjOb
    3Bj1FBAAhfr+g6Ug8VST061TsG4DhSEbPRPElW0/6YGH74582CoK1M49pY1dBH5m
    ajUzhy67Bez5lpgQmUKD5dzeQppiOzV9PqIOZft3hvBbATaV9YZz6QmPVue/m6Dx
    LaaHunyVSkCwctT2mFkO2cxcweCqv8qLkyoysmGx34F5aGWPXLccrwryWO8hcUDU
    1g2KnjEMU6OaPPeN5rLWKy8Trcw0UrtCMmtZQ7kys/sB/bUWiJJE9yWAn5PIoJIs
    EdDJGZEP9yVHi145HvLhrE+34K/ddtI0C8IUDBvZSeNB99sGbdjtG6p6RWZpUQDu
    BNgp16cTmIDTp96LaNq4E6OuHQoBSCBca4Ly6lZRY72bjOx0+zM/gO19qfI3asL6
    Mk2cDkb8F+g4QL5o95EXbbjkhgDS4j+zw3UPgNW3i5YbNf6IRr+x8y/fN37GO5X5
    5JL1S73s5W0Bv/DIJAVzHYdbxiEFBiIHqMskgAOl2gYoIADHNNvAgZoLGlv6F50x
    mQRG7bFCrsGbHXg+h9Mb8InNC+ZYWQzgr6OVAUXAKD//DEMpp3qd1Tw/kAGzWaw7
    6BK5dw7v/1W32pqH1FqtmOszBgIRbdX5iOCisD+IyaW/+ae/veCyd7zsWu7QtvXj
    tFK8TMwe+gRCn0fNziCUMH2oRrAMETuQcNIcXpCu6qbiqy2pxE0=
    =4h7y
    -----END PGP SIGNATURE----- (+ assets)

---
+++ Resources/public/static/js/ee2540b454ff88a88ca2.js
@@ -0,0 +1,1 @@
+(this.webpackJsonpAdministration=this.webpackJsonpAdministration||[]).push([[496],{e1vH:function(t,e,i){"use strict";i.r(e),e.default={props:{itemToReplace:{type:Object,required:!0}},data:function(){return{multiSelect:!1}},methods:{getMediaEntityForUpload:function(){return this.itemToReplace},cleanUpFailure:function(t,e){this.createNotificationError({message:e})}}}}}]);
