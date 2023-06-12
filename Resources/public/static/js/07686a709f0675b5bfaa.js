09a1a4749b2943f06e9cf6e0b2bb8feacd1b0b0f
shopwareBot <shopwarebot@shopware.com>
2023-06-12 09:33:23 +0000

    v6.5.2.0 (+ assets)

---
+++ Resources/public/static/js/07686a709f0675b5bfaa.js
@@ -0,0 +1,1 @@
+(this.webpackJsonpAdministration=this.webpackJsonpAdministration||[]).push([[524],{"Yd/I":function(s,n,t){"use strict";t.r(n);n.default={template:'\n{% block sw_cms_block_sidebar_filter %}\n<div class="sw-cms-block-sidebar-filter">\n    <slot name="content"></slot>\n</div>\n{% endblock %}\n'}}}]);
