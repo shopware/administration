12c78ab1d93bb197764749c834867aeaadb63147
shopwareBot <shopwarebot@shopware.com>
2023-06-12 14:33:44 +0000

    v6.5.2.1 (+ assets)

---
+++ Resources/public/static/js/a7d568177d6d83379aa9.js
@@ -0,0 +1,1 @@
+(this.webpackJsonpAdministration=this.webpackJsonpAdministration||[]).push([[612],{dkhs:function(t,e,s){"use strict";s.r(e),e.default={data:function(){return{propertyNames:{label:this.$tc("sw-settings-custom-field.customField.detail.labelLabel"),placeholder:this.$tc("sw-settings-custom-field.customField.detail.labelPlaceholder")}}}}}}]);
