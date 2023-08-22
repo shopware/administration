4c324c2c3a004fc38bc0fead5ae67fe02a11e7c1
shopwareBot <shopwarebot@shopware.com>
2023-08-22 12:16:43 +0000

    v6.5.4.1 (+ assets)

---
+++ Resources/public/static/js/6d49adbede0256c80c12.js
@@ -0,0 +1,1 @@
+(this.webpackJsonpAdministration=this.webpackJsonpAdministration||[]).push([[544],{Rq5j:function(n,t,i){"use strict";i.r(t);t.default={template:'\n{% block sw_first_run_wizard_paypal_base %}\n<router-view v-on="$listeners" />\n{% endblock %}\n'}}}]);
