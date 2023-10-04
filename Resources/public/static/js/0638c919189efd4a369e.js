ab6f136d2d0feebd250eadf6a1624fcff0c10956
shopwareBot <shopwarebot@shopware.com>
2023-10-04 13:09:21 +0000

    v6.5.6.0 (+ assets)

---
+++ Resources/public/static/js/0638c919189efd4a369e.js
@@ -0,0 +1,1 @@
+(this.webpackJsonpAdministration=this.webpackJsonpAdministration||[]).push([[545],{fCTa:function(n,s,t){"use strict";t.r(s);s.default={template:'\n{% block sw_first_run_wizard_shopware_base %}\n<router-view v-on="$listeners" />\n{% endblock %}\n'}}}]);
