7feb4dddac1114ca1a17b9ccc29296c90acb02a2
shopwareBot <shopwarebot@shopware.com>
2024-02-07 10:08:08 +0000

    v6.5.8.3 (+ assets)

---
+++ Resources/public/static/js/00596bca5b3e6f16da05.js
@@ -0,0 +1,1 @@
+(this.webpackJsonpAdministration=this.webpackJsonpAdministration||[]).push([[609],{v7YX:function(t,e,s){"use strict";s.r(e),e.default={data:function(){return{propertyNames:{label:this.$tc("sw-settings-custom-field.customField.detail.labelLabel"),helpText:this.$tc("sw-settings-custom-field.customField.detail.labelHelpText")}}}}}}]);
