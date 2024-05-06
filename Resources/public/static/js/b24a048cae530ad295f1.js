562442b681ccfc2b83cfbaf6c1559004c47486a7
shopwareBot <shopwarebot@shopware.com>
2024-05-06 12:32:18 +0000

    v6.6.2.0 (+ assets)

---
+++ Resources/public/static/js/b24a048cae530ad295f1.js
@@ -0,0 +1,1 @@
+"use strict";(window.webpackJsonpAdministration=window.webpackJsonpAdministration||[]).push([[73089],{73089:function(n,t,o){o.r(t),o.d(t,{default:function(){return s}});var s={template:'<div class="sw-cms-block-html">\n    <slot name="content">\n        \n        {% block sw_cms_block_html_slot_html_content %}{% endblock %}\n    </slot>\n</div>\n'}}}]);
