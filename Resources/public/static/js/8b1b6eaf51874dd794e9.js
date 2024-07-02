def066979696a969dd2037aecd6dd7e0315235ac
shopwareBot <shopwarebot@shopware.com>
2024-07-02 13:25:25 +0000

    v6.6.4.0 (+ assets)

---
+++ Resources/public/static/js/8b1b6eaf51874dd794e9.js
@@ -0,0 +1,6 @@
+(window.webpackJsonpAdministration=window.webpackJsonpAdministration||[]).push([[22791],{664295:function(){},922791:function(t,i,e){"use strict";e.r(i),e.d(i,{default:function(){return o}}),e(912895);var o={template:'<div class="sw-cms-preview-html">\n    <sw-code-editor\n        v-model:value="demoValue"\n        :disabled="true"\n        :editor-config="editorConfig"\n    />\n</div>\n',data(){return{demoValue:`
+<h2>Lorem ipsum dolor</h2>
+<p>Lorem ipsum dolor sit amet</p>
+<button type="button">
+    Click me!
+</button>`.trim(),editorConfig:{highlightActiveLine:!1,cursorStyle:"slim",highlightGutterLine:!1,showFoldWidgets:!1}}}}},912895:function(t,i,e){var o=e(664295);o.__esModule&&(o=o.default),"string"==typeof o&&(o=[[t.id,o,""]]),o.locals&&(t.exports=o.locals),e(745346).Z("21d91cb9",o,!0,{})}}]);
