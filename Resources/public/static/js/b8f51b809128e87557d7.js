f69db942535b66f740648ff36bc4380303133997
shopwareBot <shopwarebot@shopware.com>
2024-08-06 08:00:54 +0000

    v6.6.5.0 (+ assets)

---
+++ Resources/public/static/js/b8f51b809128e87557d7.js
@@ -0,0 +1,1 @@
+"use strict";(window.webpackJsonpAdministration=window.webpackJsonpAdministration||[]).push([[75974],{875974:function(t,e,i){i.r(e),e.default={data(){return{propertyNames:{label:this.$tc("sw-settings-custom-field.customField.detail.labelLabel"),helpText:this.$tc("sw-settings-custom-field.customField.detail.labelHelpText")}}}}}}]);
---
+++ Resources/public/static/js/d706cc43b834cf97e7cb.js
@@ -0,0 +1,1 @@
+"use strict";(window.webpackJsonpAdministration=window.webpackJsonpAdministration||[]).push([[76471],{976471:function(t,e,i){i.r(e),e.default={data(){return{propertyNames:{label:this.$tc("sw-settings-custom-field.customField.detail.labelLabel"),placeholder:this.$tc("sw-settings-custom-field.customField.detail.labelPlaceholder")}}}}}}]);
