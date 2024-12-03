e94f2b2c96730fe4ba15b995de706d19a300df4f
shopwareBot <shopwarebot@shopware.com>
2024-12-03 09:53:21 +0000

    v6.6.9.0 (+ assets)

---
+++ Resources/public/static/js/5c9d02ab1944134a769e.js
@@ -0,0 +1,1 @@
+"use strict";(window.webpackJsonpAdministration=window.webpackJsonpAdministration||[]).push([[1818],{901818:function(n,o,t){t.r(o),t.d(o,{default:function(){return i}});var i={template:'\n{% block sw_cms_block_product_slider %}\n<div class="sw-cms-block-product-slider">\n    <slot name="productSlider"></slot>\n</div>\n{% endblock %}\n',compatConfig:Shopware.compatConfig}}}]);
