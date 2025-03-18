fe7c57c8f489f56218dcd0854298eb675dbfc51d
shopwareBot <shopwarebot@shopware.com>
2025-03-18 14:50:56 +0000

    chore/remove-trait-from-feature-test (+ assets)

---
+++ Resources/public/administration/assets/index-Bjc95_v9.js
@@ -0,0 +1,1 @@
+const t='{% block sw_alert %} <mt-banner v-bind="$attrs" > <template #default> <slot></slot> </template> </mt-banner> {% endblock %}',{Component:e}=Shopware;e.register("sw-alert",{template:t});
---
+++ Resources/public/administration/assets/index-DCZ1QgxK.js
@@ -0,0 +1,1 @@
+const t='{% block sw_context_menu %} <div class="sw-context-menu"> {% block sw_context_menu_content %} <div class="sw-context-menu__content"> <slot> {% block sw_context_menu_slot_default %}{% endblock %} </slot> </div> {% endblock %} </div> {% endblock %}',{Component:e}=Shopware;e.register("sw-context-menu",{template:t});
