8aa8ebd7cbf77bd5fc97cd35e8fe1e6bd4828aa0
shopwareBot <shopwarebot@shopware.com>
2025-03-24 10:37:24 +0000

    v6.7.0.0-rc1 (+ assets)

---
+++ Resources/public/administration/assets/order.types-BvKP5qzO.js
@@ -0,0 +1,1 @@
+var o=(r=>(r.PRODUCT="product",r.CREDIT="credit",r.CUSTOM="custom",r.PROMOTION="promotion",r.CONTAINER="container",r))(o||{}),a=(r=>(r.ABSOLUTE="absolute",r.QUANTITY="quantity",r))(a||{});export{o as L,a as P};
