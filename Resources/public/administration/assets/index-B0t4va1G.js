fac9ce4ac474b05e083891969c464c3d587b7f08
shopwareBot <shopwarebot@shopware.com>
2025-03-27 18:53:44 +0000

    chore/remove-group-not-deterministic-from-tests (+ assets)

---
+++ Resources/public/administration/assets/index-B0t4va1G.js
@@ -0,0 +1,1 @@
+const{Component:i}=Shopware;i.extend("sw-condition-is-net-select","sw-condition-operator-select",{computed:{operator:{get(){return this.condition.value?this.condition.value.isNet:null},set(t){this.condition.value||(this.condition.value={}),this.condition.value={...this.condition.value,isNet:t}}}}});
