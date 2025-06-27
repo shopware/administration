a08a7a2e4b9b74b02e09cb673089c4a2607bc95b
shopwareBot <shopwarebot@shopware.com>
2025-06-27 09:10:56 +0000

    ci-fix-split (+ assets)

---
+++ Resources/public/administration/assets/notifications.service-B8e4VE1b.js
@@ -0,0 +1,1 @@
+import{A as s}from"./api.service-B_mKw9kM.js";import"./channel-CphEkTiM.js";class o extends s{constructor(t,e){super(t,e,null,"application/json"),this.name="notificationsService"}fetchNotifications(t,e=null){return this.httpClient.get("notification/message",{params:{limit:t,latestTimestamp:e},headers:this.getBasicHeaders()}).then(({data:i})=>i)}}export{o as default};
---
+++ Resources/public/administration/assets/user-input-sanitize.service-CSbjiTI2.js
@@ -0,0 +1,1 @@
+import{A as i}from"./api.service-B_mKw9kM.js";import"./channel-CphEkTiM.js";class p extends i{constructor(e,t){super(e,t,null,"application/json"),this.name="userInputSanitizeService"}sanitizeInput({html:e,field:t}){return this.httpClient.post("_admin/sanitize-html",{html:e,field:t??null},{headers:this.getBasicHeaders()}).then(s=>i.handleResponse(s))}}export{p as default};
