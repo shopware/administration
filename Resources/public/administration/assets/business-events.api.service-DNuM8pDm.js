7ebf0f26a59873d1f452170a60ccad9e5a9716c5
shopwareBot <shopwarebot@shopware.com>
2025-07-04 08:43:21 +0000

    fix/flaky-search-keyword-updater-test (+ assets)

---
+++ Resources/public/administration/assets/business-events.api.service-DNuM8pDm.js
@@ -0,0 +1,1 @@
+import{A as n}from"./api.service-B_mKw9kM.js";import"./channel-CphEkTiM.js";class p extends n{constructor(e,s,t="business-events"){super(e,s,t),this.name="businessEventService"}getBusinessEvents(e={},s={}){const t=e,i=this.getBasicHeaders(s);return this.httpClient.get("/_info/events.json",{params:t,headers:i}).then(r=>n.handleResponse(r))}}export{p as default};
