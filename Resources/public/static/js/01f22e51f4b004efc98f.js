b281d4f9aab30413a8a017d05b2d56c9c86ba2fa
shopwareBot <shopwarebot@shopware.com>
2025-10-21 09:04:57 +0000

    v6.6.10.7 (+ assets)

---
+++ Resources/public/static/js/01f22e51f4b004efc98f.js
@@ -0,0 +1,6 @@
+(window.webpackJsonpAdministration=window.webpackJsonpAdministration||[]).push([[87980],{853293:function(){},187980:function(e,t,i){"use strict";i.r(t),i.d(t,{default:function(){return o}}),i(597446);var o={template:'{% block sw_cms_el_preview_html %}\n<div class="sw-cms-el-preview-html">\n    <sw-code-editor\n        v-model:value="demoValue"\n        :disabled="true"\n        :editor-config="editorConfig"\n    />\n</div>\n{% endblock %}\n',compatConfig:Shopware.compatConfig,data(){return{demoValue:`
+<h2>Lorem ipsum</h2>
+<p>Lorem ipsum</p>
+<button type="button">
+    Click me!
+</button>`.trim(),editorConfig:{highlightActiveLine:!1,cursorStyle:"slim",highlightGutterLine:!1,showFoldWidgets:!1}}}}},597446:function(e,t,i){var o=i(853293);o.__esModule&&(o=o.default),"string"==typeof o&&(o=[[e.id,o,""]]),o.locals&&(e.exports=o.locals),(0,i(370534).A)("e006a47c",o,!0,{})}}]);
