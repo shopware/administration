2f5b4ce0c586ce2a79d3b706632ab2d09170a4aa
shopwareBot <shopwarebot@shopware.com>
2025-12-09 10:07:30 +0000

    v6.7.5.1 (+ assets)

---
+++ Resources/public/administration/assets/app-cms-blocks.service-DWhPAGXK.js
@@ -0,0 +1,1 @@
+import{A as t}from"./api.service-C2HzZe9z.js";import"./channel-DNXkJugl.js";class o extends t{constructor(s,e){super(s,e,null,"application/json"),this.name="appCmsBlocks"}fetchAppBlocks(){return this.httpClient.get("app-system/cms/blocks",{headers:this.getBasicHeaders()}).then(({data:s})=>s.blocks)}}export{o as default};
