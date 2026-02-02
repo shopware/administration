00ae187f066c204d3a58a61c396c803dad509277
shopwareBot <shopwarebot@shopware.com>
2026-02-02 12:45:44 +0000

    v6.7.7.0 (+ assets)

---
+++ Resources/public/administration/assets/index-BMrGkIgD.js
@@ -0,0 +1,1 @@
+const e={data(){return{propertyNames:{label:this.$tc("sw-settings-custom-field.customField.detail.labelLabel"),placeholder:this.$tc("sw-settings-custom-field.customField.detail.labelPlaceholder"),helpText:this.$tc("sw-settings-custom-field.customField.detail.labelHelpText")}}}};export{e as default};
