83edbb9f715b7eae173c11729451d8b56a1dbd94
shopwareBot <shopwarebot@shopware.com>
2026-02-18 11:48:57 +0000

    fix/nightly-flaky-integration-tests (+ assets)

---
+++ Resources/public/administration/assets/index-C4XG56XL.js
@@ -0,0 +1,1 @@
+const i={props:{itemToReplace:{type:Object,required:!0}},data(){return{multiSelect:!1}},methods:{getMediaEntityForUpload(){return this.itemToReplace},cleanUpFailure(t,e){this.createNotificationError({message:e})}}};export{i as default};
