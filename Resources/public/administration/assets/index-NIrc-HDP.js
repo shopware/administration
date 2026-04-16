c278a67e1d7cd5cfbefd1a30aa60d1baf38411cd
shopwareBot <shopwarebot@shopware.com>
2026-04-16 13:33:55 +0000

    v6.7.9.0 (+ assets)

---
+++ Resources/public/administration/assets/index-NIrc-HDP.js
@@ -0,0 +1,1 @@
+const o='{% block sw_cms_block_sidebar_category_navigation %} <div class="sw-cms-block-sidebar-category-navigation"> <slot name="content"></slot> </div> {% endblock %}',t={template:o};export{t as default};
---
+++ Resources/public/administration/assets/index-TCq7iTEh.js
@@ -0,0 +1,1 @@
+const t='{% block sw_cms_block_image_text_cover %} <div class="sw-cms-block-image-text-cover"> <slot name="left"></slot> <slot name="right"></slot> </div> {% endblock %}',e={template:t};export{e as default};
