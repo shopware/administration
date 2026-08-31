244702cde25a637c14bdeb11885bdca0957e5dda
shopwareBot <shopwarebot@shopware.com>
2026-08-31 11:38:40 +0000

    test/nightly-fix-test-kernel-spurious-boot (+ assets)

---
+++ Resources/public/administration/assets/index-8kYWW6TC.js
@@ -0,0 +1,1 @@
+const e='{% block sw_cms_element_product_box_preview %} <div class="sw-cms-el-preview-product-box"> <sw-cms-product-box-preview /> </div> {% endblock %}',c={template:e};export{c as default};
